efe1b5ca2117f4557c97aff657056c3dd8257033
Sunny0022 <Sunny0022@users.noreply.github.com>
2020-04-08 18:16:37 +0530

    Add files via upload

---
+++ csvReader.rb
@@ -0,0 +1,44 @@
+require 'csv'
+class CSVReader
+  def read(file)
+    CSV.foreach(file,  headers: true) do |row|
+      @name = row[0]
+      @empId = row[1]
+      @designation = row[2]
+      Employee.new(@name, @empId, @designation)
+      #Employee.new(row[0], row[1], row[2])
+    end
+  end
+end
+class Employee
+
+  attr_accessor :name, :empId, :designation
+
+  def initialize(name, empId, designation)
+    @name = name
+    @empId = empId
+    @designation = designation
+  end
+
+  def self.export_file_into_csv
+    data = Array.new
+    ObjectSpace.each_object(self) do |obj|
+      data << [obj.name, obj.empId, obj.designation]
+    end
+    body = data.sort_by! { |row| row[2] }
+    grouped_details = body.group_by { |row| row[2] }
+    details = grouped_details.each do |key, value|  
+      value.sort_by! { |row| row[1] }
+    end
+    File.open('employees.csv', 'w') do |file|
+      details.each do |designation, value|
+        file.puts "#{designation}#{"s" if details[designation].length > 1}"
+        value.each { |detail| file.puts "#{detail[0]} (EmpId: #{detail[1]})" }
+      file.puts
+      end
+    end
+  end
+end
+new_csv = CSVReader.new
+new_csv.read('test.csv') # instance method
+Employee.export_file_into_csv # class method
